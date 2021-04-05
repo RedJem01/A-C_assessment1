52ff04f33475ac53c5a2fed787dbc3622757b07b
Jem <79197456+RedJem01@users.noreply.github.com>
2021-04-05 17:38:42 +0100

    Added the 2048 files and made it all easier to find or edit

--- Program.cs
+++ Program.cs
@@ -19,35 +19,45 @@ namespace A_C_assessment1
             sorting so = new sorting();
             searching se = new searching();
 
+            //reading files in
             r.read();
 
+            //sorting files
             Console.WriteLine("Road 1 (256):");
             r.output(r.Road_1);
-
             (List<int> asortedRoad_1, List<int> dsortedRoad_1) = so.sort(r.Road_1);
 
             Console.WriteLine("Road 2 (256):");
             r.output(r.Road_2);
-
             (List<int> asortedRoad_2, List<int> dsortedRoad_2) = so.sort(r.Road_2);
 
             Console.WriteLine("Road 3 (256):");
             r.output(r.Road_3);
-
             (List<int> asortedRoad_3, List<int> dsortedRoad_3) = so.sort(r.Road_3);
 
+            Console.WriteLine("Road 1 (2048):");
+            r.output(r.Road_1_);
+            (List<int> asortedRoad_1_, List<int> dsortedRoad_1_) = so.sort(r.Road_1_);
+
+            Console.WriteLine("Road 2 (2048):");
+            r.output(r.Road_2_);
+            (List<int> asortedRoad_2_, List<int> dsortedRoad_2_) = so.sort(r.Road_2_);
+
+            Console.WriteLine("Road 3 (2048):");
+            r.output(r.Road_3_);
+            (List<int> asortedRoad_3_, List<int> dsortedRoad_3_) = so.sort(r.Road_3_);
+
+        
             try
             {
-
-
-                Console.WriteLine("Which road did you want to look at?");
+                Console.WriteLine("Which road did you want to look at? 1,2,3 are 256 4,5,6 are 2048");
                 string choice = Console.ReadLine();
                 if (choice == "1")
                 {
                     string found = se.search(asortedRoad_1);
-                    if (found == "Error: The number you searched for is not in the list")
+                    if (se.inPosition == false)
                     {
-                        Console.WriteLine(found);
+                        Console.WriteLine($"The number you searched for is not in the list. The closest number is {found} and it is in the position {se.position}");
                     }
                     else
                     {
@@ -57,9 +67,9 @@ namespace A_C_assessment1
                 else if (choice == "2")
                 {
                     string found = se.search(asortedRoad_2);
-                    if (found == "Error: The number you searched for is not in the list")
+                    if (se.inPosition == false)
                     {
-                        Console.WriteLine(found);
+                        Console.WriteLine($"The number you searched for is not in the list. The closest number is {found} and it is in the position {se.position}");
                     }
                     else
                     {
@@ -70,16 +80,55 @@ namespace A_C_assessment1
                 else if (choice == "3")
                 {
                     string found = se.search(asortedRoad_3);
-                    if (found == "Error: The number you searched for is not in the list")
+                    if (se.inPosition == false)
                     {
-                        Console.WriteLine(found);
+                        Console.WriteLine($"The number you searched for is not in the list. The closest number is {found} and it is in the position {se.position}");
                     }
                     else
                     {
                         Console.WriteLine($"The number you searched for is in position {found}");
                     }
                 }
-            
+
+                else if (choice == "4")
+                {
+                    string found = se.search(asortedRoad_1_);
+                    if (se.inPosition == false)
+                    {
+                        Console.WriteLine($"The number you searched for is not in the list. The closest number is {found} and it is in the position {se.position}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The number you searched for is in position {found}");
+                    }
+                }
+
+                else if (choice == "5")
+                {
+                    string found = se.search(asortedRoad_2_);
+                    if (se.inPosition == false)
+                    {
+                        Console.WriteLine($"The number you searched for is not in the list. The closest number is {found} and it is in the position {se.position}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The number you searched for is in position {found}");
+                    }
+                }
+
+                else if (choice == "6")
+                {
+                    string found = se.search(asortedRoad_3_);
+                    if (se.inPosition == false)
+                    {
+                        Console.WriteLine($"The number you searched for is not in the list. The closest number is {found} and it is in the position {se.position}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The number you searched for is in position {found}");
+                    }
+                }
+
                 else
                 {
                     throw new WrongInputException("That is the wrong input. Please input 1, 2 or 3.");
@@ -91,6 +140,7 @@ namespace A_C_assessment1
                 Console.WriteLine(e.Message);
             }
 
+            merge();
         }
     }
 }
---
+++ Roads.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace A_C_assessment1
+{
+    class Roads
+    {
+        private List<int> _Road_1;
+        public List<int> Road_1
+        {
+            get { return _Road_1; }
+            set { _Road_1 = value; }
+        }
+
+        private List<int> _Road_2;
+        public List<int> Road_2
+        {
+            get { return _Road_2; }
+            set { _Road_2 = value; }
+        }
+
+        private List<int> _Road_3;
+        public List<int> Road_3
+        {
+            get { return _Road_3; }
+            set { _Road_3 = value; }
+        }
+
+        private int _count;
+        public int count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+        private List<int> _Road_1_;
+        public List<int> Road_1_
+        {
+            get { return _Road_1_; }
+            set { _Road_1_ = value; }
+        }
+
+        private List<int> _Road_2_;
+        public List<int> Road_2_
+        {
+            get { return _Road_2_; }
+            set { _Road_2_ = value; }
+        }
+
+        private List<int> _Road_3_;
+        public List<int> Road_3_
+        {
+            get { return _Road_3_; }
+            set { _Road_3_ = value; }
+        }
+
+        private List<int> _merge256;
+        public List<int> merge256
+        {
+            get { return _merge256; }
+            set { _merge256 = value; }
+        }
+
+        private List<int> _merge2048;
+        public List<int> merge2048
+        {
+            get { return _merge2048; }
+            set { _merge2048 = value; }
+        }
+
+
+        public Roads()
+        {
+            Road_1 = new List<int>();
+            Road_2 = new List<int>();
+            Road_3 = new List<int>();
+            Road_1_ = new List<int>();
+            Road_2_ = new List<int>();
+            Road_3_ = new List<int>();
+            merge256 = new List<int>();
+            merge2048 = new List<int>();
+        }
+
+        public void output(List<int> list)
+        {
+            list.ForEach(Console.WriteLine);
+        }
+        public void read()
+        {
+            string path = Path.GetFullPath("Road_1_256.txt");   //Gets the file path for the text file
+            string[] text = File.ReadAllLines(path);    //Reads all the data in the text file to a varaible
+            for (int i = 0; i < text.Length; i++)   //Loops through the data in the text file 
+            {
+                Road_1.Add(int.Parse(text[i]));    //Adds each number in it to a list
+            }
+
+            path = Path.GetFullPath("Road_2_256.txt");
+            text = File.ReadAllLines(path);
+            for (int i = 0; i < text.Length; i++)
+            {
+                Road_2.Add(int.Parse(text[i]));
+            }
+
+            path = Path.GetFullPath("Road_3_256.txt");
+            text = File.ReadAllLines(path);
+            for (int i = 0; i < text.Length; i++)
+            {
+                Road_3.Add(int.Parse(text[i]));
+            }
+
+            path = Path.GetFullPath("Road_1_2048.txt");
+            text = File.ReadAllLines(path);
+            for (int i = 0; i < text.Length; i++)
+            {
+                Road_1_.Add(int.Parse(text[i]));
+            }
+
+
+            path = Path.GetFullPath("Road_2_2048.txt");
+            text = File.ReadAllLines(path);
+            for (int i = 0; i < text.Length; i++)
+            {
+                Road_2_.Add(int.Parse(text[i]));
+            }
+
+
+            path = Path.GetFullPath("Road_3_2048.txt");
+            text = File.ReadAllLines(path);
+            for (int i = 0; i < text.Length; i++)
+            {
+                Road_3_.Add(int.Parse(text[i]));
+            }
+        }
+        
+        public void merge()
+        {
+
+        }
+    }
+}
